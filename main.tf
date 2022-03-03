312f9c24a0249a2a938427aa0e46ae90805c5b50
Jan Kalbantner <jan.kalbantner@wechselgott.com>
2022-03-03 13:20:11 +0000

    fix errors for newer tf version
    Signed-off-by: Jan Kalbantner <jan.kalbantner@wechselgott.com>

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
 resource "null_resource" "deploy_route53_backup_and_restore" {
   triggers = {
     build = timestamp()
+    interval = var.interval
+    retention_period = var.retention_period
+    region = var.region
+    aws_profile = var.aws_profile
   }
 
   provisioner "local-exec" {
     working_dir = path.module
-    command     = "npm i && sls deploy --backup-interval ${var.interval} --retention-period ${var.retention_period} --region ${var.region} --aws-profile ${var.aws_profile}"
+    command     = "npm i && sls deploy --backup-interval '${var.interval}' --retention-period '${var.retention_period}' --region '${var.region}' --aws-profile '${var.aws_profile}'"
   }
 }
 
 resource "null_resource" "remove_route53_backup_and_restore" {
   provisioner "local-exec" {
-    when        = "destroy"
+    when        = destroy
     working_dir = path.module
-    command     = "npm i && sls remove --backup-interval ${var.interval} --retention-period ${var.retention_period} --region ${var.region} --aws-profile ${var.aws_profile}"
+    command     = "npm i && sls remove --backup-interval '${self.triggers.interval}' --retention-period '${self.triggers.retention_period}' --region '${self.triggers.region}' --aws-profile '${self.triggers.aws_profile}'"
   }
 }
